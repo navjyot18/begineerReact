75cafba95d0e2777a73854f3203172551bc8f296
bhelenavjyot18@gmail.com <bhelenavjyot18@gmail.com>
2021-03-31 00:16:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <img src="https://avatars0.githubusercontent.com/u/9892522?v=4&s=400" />
+    <h1>React DEMO Project</h1>
+      <h3>
+React is an open-source, front end, JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications
+    </h3>
+
+    </div>
+  );
+}
+
+export default App;
